50e32e61bbfd63436eb4d2f55f3f05aa418121bd
mariojaimes <54184058+mariojaimes@users.noreply.github.com>
2019-10-17 00:00:25 -0500

    Update Problema2_13.py

--- Problema2_13.py
+++ Problema2_13.py
@@ -1,20 +1,17 @@
 MAT=int(input("Dame la matricula del alumno :"))
-CARR=input("Carrera del alumno  
-1=Economia,
-2=Computacion,
-3=contabilidad,
-4=Administracion :")
+CARR=int(input("Carrera del alumno (0 Economia, 1 Computacion, 2 Contabilidad, 3 Administracion):"))
 SEM=int(input("En que semestre esta :"))
-if CARR==1:
+PROM= float(input("Dame el promedio del alumno"))
+if CARR==0:
     if SEM >= 6 and PROM>=8.8:
         print(f"El alumno conmatricula{MAT} incrito en {CARR} es aceptado")
-if CARR==2:
+if CARR==1:
     if SEM >= 6 and PROM>=8.5:
         print(f"El alumno conmatricula{MAT} incrito en {CARR} es aceptado")
-if CARR==3:
+if CARR==2:
     if SEM >= 5 and PROM>=8.5:
         print(f"El alumno conmatricula{MAT} incrito en {CARR} es aceptado")
-if CARR==4:
+if CARR==3:
     if SEM >= 5 and PROM>=8.5:
         print(f"El alumno conmatricula{MAT} incrito en {CARR} es aceptado")
 print("Fin del programa")

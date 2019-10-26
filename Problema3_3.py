16a0b0685a5b61f17ac20bf956c6385c388f8b0e
mariojaimes <54184058+mariojaimes@users.noreply.github.com>
2019-10-25 23:54:17 -0500

    Update Problema3_3.py

--- Problema3_3.py
+++ Problema3_3.py
@@ -1,1 +1,12 @@
-.
+serie = 0
+n = float(input("Dame un numero entero positivo"))
+band == True
+i = 1
+for i in range(1 <= n):
+    if band ==True:
+        serie =serie + 1 / i # serie = serie + 1 / i
+        i = i + 1 # i=i + 1
+    else:
+        serie = serie - 1 / i# serie=serie - 1 / i
+        i = i + 1 # i = i +1
+print(f"{serie}")

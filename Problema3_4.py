4cc40a5435e3fac6f69c42434251633cc6ac8208
mariojaimes <54184058+mariojaimes@users.noreply.github.com>
2019-10-26 00:24:51 -0500

    Update Problema3_4.py

--- Problema3_4.py
+++ Problema3_4.py
@@ -1,1 +1,13 @@
-.
+NOM=0
+SUE=float(input("Dame el sueldo del trabajador: "))
+for i in range (1,11,1):
+    if SUE <> -1:
+        if SUE < 1000:
+        NSUE= SUE*1.15
+        else:
+        NSUE=SUE*1.12
+    NOM=NOM+NSUE
+    print(f"El nuevo sueldo es {NSUE}")
+    else:
+        print(f"{NOM}")
+print("Fin del programa"

02038264e77dc7c912371d243907304f1cb717cf
mariojaimes <54184058+mariojaimes@users.noreply.github.com>
2019-10-25 23:52:53 -0500

    Update Problema3_2.py

--- Problema3_2.py
+++ Problema3_2.py
@@ -1,1 +1,13 @@
-.
+band== True
+sumser= 0
+i=2
+while(1<= 1800):
+    sumser = sumser + 1 # sumser = sumser + 1
+    print(f"{i}")
+if band == True:
+    band== False
+    i = i + 3 #i = i + 3
+else:
+    band == True
+    i = 1 + 2 # i = i + 2
+print(f"{sumser}")

4805aded4f7ea86ad8fb2e53974c40f85a33d330
mariojaimes <54184058+mariojaimes@users.noreply.github.com>
2019-11-05 17:52:44 -0600

    Add files via upload

---
+++ Diccionarios.py
@@ -0,0 +1,13 @@
+#diccionarios{'llave':'valor'}
+alumno=('num_cta'='113006070',
+        'nombre'='Mario'
+        'paterno'='Jaimes'
+        'semestre'=1,
+        'promedio'=0.0
+        'materias'=['CYP','Alg','Calc''Geomet','intro ico'],
+        'regular'=True,
+        'Lagrimas_por_examen'= 5
+        }
+        
+print(alumno['nombre'])
+print(alumno)

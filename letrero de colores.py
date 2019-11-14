662020230f949aa543fa5be3bca859ffd5510dfb
mariojaimes <54184058+mariojaimes@users.noreply.github.com>
2019-11-13 22:36:19 -0600

    Add files via upload

---
+++ letrero de colores.py
@@ -0,0 +1,7 @@
+#pip install colorama
+from colorama import Fore, Back, Style
+print(Fore.YELLOW + 'Universidad Nacional Autonoma de Mexico')
+print(Fore.BLUE +'FES Aragon')
+print(Fore.GREEN +'Jaimes Martinez Mario Alberto')
+print(Style.RESET_ALL)
+print ("Fin del programa")
